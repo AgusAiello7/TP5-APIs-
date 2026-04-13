b1768c45602110568245f081f7c57fe061d353bb
PEDRO MALOWICKI SANCHO <49552421@ort>
2026-04-13 12:08:34 -0300

    primeros cambios

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hola")
+async function buscarPokemonNombre(){
+    let nombrePokemon = document.getElementById("nombrePokemon").value
+    const objPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`) 
+  
+}
