cc42b6606fd9e0da4d5820eecfecc3780288ebed
PEDRO MALOWICKI SANCHO <49552421@ort>
2026-04-15 14:28:34 -0300

    Anda todo, pero la api de Pokemon esta bloqueada

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 async function buscarPokemonNombre(){
     let nombrePokemon = document.getElementById("nombrePokemon").value
-    const objPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`) 
-  
+    try{
+    const res = await fetch(`http://www.omdbapi.com/?i=${nombrePokemon}&apikey=f2885c6e`)
+    const obj = await res.json()
+    console.log(obj)
+
+    document.getElementById("info1Pokemon").innerHTML = `Año: ${obj.Year}`
+    document.getElementById("info2Pokemon").innerHTML = `Duración: ${obj.Runtime}`
+    document.getElementById("info3Pokemon").innerHTML = `Director: ${obj.Director}`
+    } catch (error) {
+        console.error('Error', error)
+    }
+
 }
+
+
+//const objPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`) 
