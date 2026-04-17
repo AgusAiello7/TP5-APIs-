60ed555b46f3c536804dc789c24e543c564c2cf1
PEDRO MALOWICKI SANCHO <49552421@ort>
2026-04-17 09:32:34 -0300

    terminado

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 async function buscarPokemonNombre(){
-    let nombrePokemon = document.getElementById("nombrePokemon").value
+    const nombrePokemon = document.getElementById("nombrePokemon").value
+    const loader =  document.getElementById("loader")
+
     try{
-    const res = await fetch(`http://www.omdbapi.com/?i=${nombrePokemon}&apikey=f2885c6e`)
+    loader.style.display = "block"
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`) 
     const obj = await res.json()
     console.log(obj)
 
-    document.getElementById("info1Pokemon").innerHTML = `Año: ${obj.Year}`
-    document.getElementById("info2Pokemon").innerHTML = `Duración: ${obj.Runtime}`
-    document.getElementById("info3Pokemon").innerHTML = `Director: ${obj.Director}`
-    } catch (error) {
+    let tipos = ""
+    for(let i = 0; i<obj.types.length; i++)
+    {
+        tipos = tipos + `${obj.types[i].type.name}, `
+    }
+
+    loader.style.display = "none"
+    document.getElementById("info1Pokemon").innerHTML = `Nombre: ${obj.name}`
+    document.getElementById("info2Pokemon").innerHTML = `Peso: ${obj.weight}`
+    document.getElementById("info3Pokemon").innerHTML = `Altura: ${obj.height}`
+    document.getElementById("info4Pokemon").innerHTML = `Tipos: ${tipos}`
+    document.getElementById("imagen").src = obj.sprites.front_default
+    } 
+    
+    catch (error) {
         console.error('Error', error)
+        loader.style.display = "block"
+        loader.innerHTML = "Error"
+        document.getElementById("imagen").src = "https://cdn-icons-png.flaticon.com/512/103/103085.png"
     }
 
 }
 
-
-//const objPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`) 
